54d9cf253bcf4733f42b19cb5b8cadef69d03de6
Onesso <odhiambofrank1965@gmail.com>
2025-07-17 08:56:24 +0300

    at the service receiving payload as a whole

--- src/app/services/course.services.ts
+++ src/app/services/course.services.ts
@@ -1,9 +1,17 @@
+import { Course } from "./../model/course";
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
 
 @Injectable({
   providedIn: "root",
 })
 export class CourseServices {
+    
   constructor(private http: HttpClient) {}
+
+  loadallCourses(): Observable<Course[]> {
+    return this.http.get<Course[]>("/api/courses/");
+  }
+
 }
